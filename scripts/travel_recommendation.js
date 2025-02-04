633a02462661e19261635b611eb88faeb9778007
ivanwe <rusev37@gmail.com>
2025-02-04 08:55:19 -0500

    complete

--- scripts/travel_recommendation.js
+++ scripts/travel_recommendation.js
@@ -2,13 +2,74 @@ function clearSearch() {
     document.querySelector('#searchInput').value = "";
 }
 
-function showResults() {
-    const resultsContainer = document.getElementById("results");
-    resultsContainer.innerHTML += '<div class="result-item">' +
-        '<img src="path/to/destination-image.jpg" alt="Destination Image" class="destination-image">' +
-        '<h2>Destination Name</h2>' +
-        '<p>This is a brief description of the destination. Explore the beauty and attractions of this amazing place.</p>' +
-        '<button class="visit-button">Visit</button>' +
-        '</div>';
-    resultsContainer.style.visibility = 'visible';
+async function showResults() {
+    let searchInputValue = document.getElementById('searchInput').value;
+    
+    const response = await fetch('/travel-recommendation/data/travel_recommendation_api.json');
+    const data = await response.json();
+
+    console.log(data);
+
+    let htmlString = '';
+    const lowerCaseInput = searchInputValue.trim().toLowerCase();
+    if (lowerCaseInput.includes('beach')) {
+        data.beaches.map((item, index) => {
+            htmlString += `<div class="result-item">
+            <img src="${item.imageUrl}" alt="Destination Image" class="destination-image">
+            <h2>${item.name}</h2>
+            <p>${item.description}</p>
+            <button class="visit-button">Visit</button>
+            </div>`;
+        });
+    }
+    else if (lowerCaseInput.includes('temple')) {
+        data.temples.map((item, index) => {
+            htmlString += `<div class="result-item">
+            <img src="${item.imageUrl}" alt="Destination Image" class="destination-image">
+            <h2>${item.name}</h2>
+            <p>${item.description}</p>
+            <button class="visit-button">Visit</button>
+            </div>`;
+        });
+    }
+    else if (lowerCaseInput.includes('countr')) {
+        data.countries.map((item, index) => {
+            let localTime = getLocalTime(item.cities[0].name, item.name);
+            htmlString += `<div class="result-item">
+            <img src="${item.cities[0].imageUrl}" alt="Destination Image" class="destination-image">
+            <h2>${item.cities[0].name}</h2>
+            <p>${item.cities[0].description}</p>
+            <button class="visit-button">Visit</button>
+            <p>Locale time: ${localTime}</p>
+            </div>`;
+        });
+    }
+    
+
+
+    const resultsContainerEl = document.getElementById('results');
+    
+    resultsContainerEl.innerHTML = htmlString;
+
+    resultsContainerEl.style.visibility = 'visible';
+    searchInputValue = '';
+}
+
+function getLocalTime(city, country) {
+    let timeZone;
+    switch(city.split(',')[0]) {
+        case 'Sydney':
+            timeZone = 'Australia/Sydney';
+            break;
+        case 'Tokyo':
+            timeZone = 'Asia/Tokyo';
+            break;
+        case 'Rio de Janeiro':
+            timeZone = 'America/Sao_Paulo';
+            break;
+        default:
+            timeZone = 'UTC';
+    }
+    const options = { timeZone: timeZone, hour12: false, hour: 'numeric', minute: 'numeric', second: 'numeric' };
+    return new Date().toLocaleTimeString('en-US', options);
 }
